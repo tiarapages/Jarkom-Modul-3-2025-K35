fa04119db19413caf278bc030d2c6f9ba2172697
tiarapages <tiaratutiprasetya@gmail.com>
2025-11-03 23:52:10 +0700

    Configure MariaDB master-slave replication
    Updated MariaDB configuration for master and slave setup, including user creation and replication settings.

--- no18.sh
+++ no18.sh
@@ -3,18 +3,17 @@
 apt update
 apt install mariadb-server mariadb-client -y
 
+service mariadb start
 # Edit Konfigurasi MariaDB untuk Master
 
 nano /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# ubah
-
-bind-address = 0.0.0.0
-
 # Tambahkan di bagian [mysqld]:
+[mysqld]
 server-id = 1
 log_bin = /var/log/mysql/mysql-bin.log
-binlog_do_db = palantir_db
+binlog_format = ROW
+bind-address = 0.0.0.0
 
 # Ubah  biar dpt diakses
 
@@ -33,19 +32,78 @@ ls -la /var/log/mysql/
 
 #login
 
-mysql -u root
+mysql
 
 #BUAT DATABASE DAN USER REPLIKASI
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root123');
+CREATE USER 'replicator'@'%' IDENTIFIED BY 'replicator123';
+GRANT REPLICATION SLAVE ON *.* TO 'replicator'@'%';
+FLUSH PRIVILEGES;
+SHOW MASTER STATUS;
+# ada tabelnya file dan posisi nya catet
+ EXIT;
 
-CREATE DATABASE palantir_db;
+### DI PALANTIR (Master) - Buat User Replikasi
 CREATE USER 'replica_user'@'%' IDENTIFIED BY 'replica_password';
 GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
 FLUSH PRIVILEGES;
-SHOW MASTER STATUS;
-SHOW DATABASES;
+EXIT;
 
-# exit
+# NARVI
+apt update
+apt install mariadb-server mariadb-client -y
 
- EXIT;
+service mariadb status
+service mariadb start
 
-### Narvi
+mysql -u root -proot123
+EXIT;
+
+nano /etc/mysql/mariadb.conf.d/50-server.cnf
+
+[mysqld]
+server-id = 2
+log_bin = /var/log/mysql/mysql-bin.log
+binlog_format = ROW
+bind-address = 0.0.0.0
+relay-log = /var/log/mysql/mysql-relay-bin
+
+mkdir -p /var/log/mysql
+chown -R mysql:mysql /var/log/mysql
+
+service mariadb restart
+
+service mariadb status
+
+#LOGIN DI NARVI (SLAVE)
+mysql -u root -proot123
+
+STOP SLAVE;
+
+CHANGE MASTER TO
+  MASTER_HOST='10.81.4.3',
+  MASTER_USER='replica_user',
+  MASTER_PASSWORD='replica_password',
+  MASTER_LOG_FILE='mysql-bin.000001',
+  MASTER_LOG_POS=328;
+
+# MASTER_LOG_FILE → hasil SHOW MASTER STATUS; di Palantir
+#MASTER_LOG_POS → hasil SHOW MASTER STATUS; di Palantir
+START SLAVE;
+
+SHOW SLAVE STATUS\G
+# pastikan Slave_IO_Running: Yes dan Slave_SQL_Running: Yes
+
+# TEST di palantir (master)
+mysql -u root -proot123
+
+CREATE DATABASE test_replication;
+USE test_replication;
+CREATE TABLE heroes (id INT PRIMARY KEY, name VARCHAR(50));
+INSERT INTO heroes VALUES (1, 'Aragorn'), (2, 'Legolas');
+
+# CEK DI NARVI (SLAVE)
+mysql -u root -proot123
+
+USE test_replication;
+SELECT * FROM heroes;
